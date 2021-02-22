965b38ef7e176693ff87cd9e10308aab6b0e995a
jonathan.lemp <jonathan.lemp@AMS-GYM-EDV1-02.gym.ams.local>
2021-02-22 15:03:58 +0100

    Here it is

--- src/basics/array.java
+++ src/basics/array.java
@@ -1,17 +1,35 @@
 package basics;
 
 public class array {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		int[] zahlen = {3, 2, 6, 9, 1, 2, 8, 5, 3, 1};
-		
-			for (int zaehler = 0; zaehler <=lenght ; zaehler = zaehler +1 ) {
-		    System.out.print(zahlen[zaehler] + " "  );  
-			
+	  public static void main(String []args) {
+		    int num, i, j, temp;
+		    Scanner input = new Scanner(System.in);
+		 
+		    System.out.println("Enter the number of integers to sort:");
+		    num = input.nextInt();
+		 
+		    int array[] = new int[num];
+		 
+		    System.out.println("Enter " + num + " integers: ");
+		 
+		    for (i = 0; i < num; i++) 
+		      array[i] = input.nextInt();
+		 
+		    for (i = 0; i < ( num - 1 ); i++) {
+		      for (j = 0; j < num - i - 1; j++) {
+		        if (array[j] > array[j+1]) 
+		        {
+		           temp = array[j];
+		           array[j] = array[j+1];
+		           array[j+1] = temp;
+		        }
+		      }
+		    }
+		 
+		    System.out.println("Sorted list of integers:");
+		 
+		    for (i = 0; i < num; i++) 
+		      System.out.println(array[i]);
+		  }
 		}
-		
-	}
-
-}
+	
